8b43ad6038c643f0c408d2ddfe78b35480695362
Patrick M Young <patrick.mckendree.young@gmail.com>
2017-05-15 10:56:14 -0600

    OSX sed wasn't running impfix correctly.

--- 2.2.0/osgeo/gdal.py
+++ 2.2.0/osgeo/gdal.py
@@ -2013,7 +2013,7 @@ class Dataset(MajorObject):
         """ Reading a chunk of a GDAL band into a numpy array. The optional (buf_xsize,buf_ysize,buf_type)
         parameters should generally not be specified if buf_obj is specified. The array is returned"""
 
-        import gdalnumeric
+        from osgeo import gdalnumeric
         return gdalnumeric.DatasetReadAsArray( self, xoff, yoff, xsize, ysize, buf_obj,
                                                buf_xsize, buf_ysize, buf_type,
                                                resample_alg = resample_alg,
@@ -2081,7 +2081,7 @@ class Dataset(MajorObject):
            Any reference to the array must be dropped before the last reference to the
            related dataset is also dropped.
         """
-        import gdalnumeric
+        from osgeo import gdalnumeric
         if xsize is None:
             xsize = self.RasterXSize
         if ysize is None:
@@ -2116,7 +2116,7 @@ class Dataset(MajorObject):
            Any reference to the array must be dropped before the last reference to the
            related dataset is also dropped.
         """
-        import gdalnumeric
+        from osgeo import gdalnumeric
         if xsize is None:
             xsize = self.RasterXSize
         if ysize is None:
@@ -2525,7 +2525,7 @@ class Band(MajorObject):
         """ Reading a chunk of a GDAL band into a numpy array. The optional (buf_xsize,buf_ysize,buf_type)
         parameters should generally not be specified if buf_obj is specified. The array is returned"""
 
-        import gdalnumeric
+        from osgeo import gdalnumeric
 
         return gdalnumeric.BandReadAsArray( self, xoff, yoff,
                                             win_xsize, win_ysize,
@@ -2538,7 +2538,7 @@ class Band(MajorObject):
                    resample_alg = GRIORA_NearestNeighbour,
                    callback = None,
                    callback_data = None):
-        import gdalnumeric
+        from osgeo import gdalnumeric
 
         return gdalnumeric.BandWriteArray( self, array, xoff, yoff,
                                            resample_alg = resample_alg,
@@ -2555,7 +2555,7 @@ class Band(MajorObject):
              Any reference to the array must be dropped before the last reference to the
              related dataset is also dropped.
           """
-          import gdalnumeric
+          from osgeo import gdalnumeric
           if xsize is None:
               xsize = self.XSize
           if ysize is None:
@@ -2578,7 +2578,7 @@ class Band(MajorObject):
              Any reference to the array must be dropped before the last reference to the
              related dataset is also dropped.
           """
-          import gdalnumeric
+          from osgeo import gdalnumeric
           if options is None:
               virtualmem = self.GetVirtualMemAuto(eAccess)
           else:
@@ -2595,7 +2595,7 @@ class Band(MajorObject):
              Any reference to the array must be dropped before the last reference to the
              related dataset is also dropped.
           """
-          import gdalnumeric
+          from osgeo import gdalnumeric
           if xsize is None:
               xsize = self.XSize
           if ysize is None:
@@ -2791,12 +2791,12 @@ class RasterAttributeTable(_object):
 
 
     def WriteArray(self, array, field, start=0):
-        import gdalnumeric
+        from osgeo import gdalnumeric
 
         return gdalnumeric.RATWriteArray(self, array, field, start)
 
     def ReadAsArray(self, field, start=0, length=None):
-        import gdalnumeric
+        from osgeo import gdalnumeric
 
         return gdalnumeric.RATReadArray(self, field, start, length)
 
